fdb6b0fb010b40e4ce4eb9dc2c98e91a14536ec6
Dennis Hartrampf <dennis.hartrampf@postcon.de>
2018-03-05 13:28:32 +0100

    Solution using streams

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+import java.util.PrimitiveIterator;
+
 public class Kata {
     public static int[] sortArray(int[] array) {
-        return array;
+        PrimitiveIterator.OfInt sortedOdds = Arrays
+                                                 .stream(array)
+                                                 .filter(Kata::isOdd)
+                                                 .sorted()
+                                                 .iterator();
+
+        return Arrays
+                   .stream(array)
+                   .map(number -> isOdd(number) ? sortedOdds.next() : number)
+                   .toArray();
+    }
+
+    private static boolean isOdd(int number) {
+        return number % 2 != 0;
     }
 }
